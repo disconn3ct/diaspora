3ae997eb222b7574b9b396dc2cb0376b6908bcad
Dis McCarthy <dis@privit.us>
2011-06-16 15:42:06 -0400

    let cap deploy the current checked-out branch by default instead of master

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,21 @@ set :scm_verbose, true
 #set :deploy_via, :checkout
 set :deploy_via, :remote_cache
 
+# Bonus! Colors are pretty!
+def red(str)
+  "\e[31m#{str}\e[0m"
+end
+
+# Figure out the name of the current local branch
+def current_git_branch
+  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
+  puts "Deploying branch #{red branch}"
+  branch
+end
+
+# Set the deploy branch to the current branch
+set :branch, current_git_branch
+
 after "deploy:setup", "deploy:create_shared_directories"
 
 namespace :deploy do
